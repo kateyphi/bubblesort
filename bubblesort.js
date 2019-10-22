294153f36b543135ba5b20ed965f621e6516021d
Katey Phillips <kateyphi@gmail.com>
2019-10-22 12:06:15 -0400

    finished

--- bubblesort.js
+++ bubblesort.js
@@ -2,14 +2,18 @@ function bubbleSort(array) {
   let count = 0
   while (count < array.length) {
     for (let i = 0; i < array.length; i++) {
-      if (array[i] > array[i + 1]) {
-        let swap = array[i]
-        array[i] = array[i + 1]
-        array[i + 1] = swap
-      }
+        swap(i,array)
     }
     count += 1
   }
 
   return array
+}
+
+function swap(i,array){
+  if (array[i] > array[i + 1]) {
+    let swap = array[i]
+    array[i] = array[i + 1]
+    array[i + 1] = swap
+  }
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,17 +1,21 @@
+
+
 describe('Bubble Sort', function(){
+  beforeAll(function () {
+
+  });
     it('handles an empty array', function(){
       expect( bubbleSort([]) ).toEqual( [] );
     });
-  });
-
-  describe('Bubble Sort', function(){
     it('handles a single element array', function(){
       expect( bubbleSort([2]) ).toEqual( [2] );
     });
-  });
-
-  describe('Bubble Sort', function(){
     it('handles a multiple element array', function(){
       expect( bubbleSort([5,50,78,2,0]) ).toEqual( [0,2,5,50,78] );
-    });
+    })
+    it('sorting an array of 5 elements involves 25 comparisons', function(){
+      spyOn(window, 'swap').and.callThrough(); 
+      bubbleSort([2,7,10,23,42])
+      expect(swap.calls.count()).toEqual(25)
+    })
   });
